b3248db7464c192dec23c74bf6bfa87852cd1bc6
snmirji1997 <snmirji1997@gmail.com>
2024-03-18 12:06:24 +0530

    sentry intigrated

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ import Express from "express";
 import * as dotenv from "dotenv";
 import db from "./dbConfig.js";
 import authHandler from "./handlers/auth.js";
+const Sentry = require("@sentry/node");
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 dotenv.config();
 
 const app = Express();
+Sentry.init({
+  dsn: "https://51a80daa310b80c497f42bd1858791f8@o4506930572558336.ingest.us.sentry.io/4506930582388736",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 
 const port = process.env.PORT || 4000;
 
